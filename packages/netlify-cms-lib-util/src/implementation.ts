3a920245298f9e7aa420712f554ba838617786b8
Dfdez <danifer.inbox@gmail.com>
2023-09-27 11:59:45 +0200

    fix: stack changes merge conflicts

--- packages/netlify-cms-lib-util/src/implementation.ts
+++ packages/netlify-cms-lib-util/src/implementation.ts
@@ -78,6 +78,7 @@ export type PersistOptions = {
   commitMessage: string;
   collectionName?: string;
   useWorkflow?: boolean;
+  publishMain?: boolean;
   unpublished?: boolean;
   status?: string;
 };
