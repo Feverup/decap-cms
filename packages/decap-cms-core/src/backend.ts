99805ce6d8a59b79da5a3e95a9ffd2eedfe4766e
Dfdez <mail.dfdez@gmail.com>
2024-11-26 13:34:51 +0100

    fix: make sure to run toFile with await

--- packages/decap-cms-core/src/backend.ts
+++ packages/decap-cms-core/src/backend.ts
@@ -1362,7 +1362,7 @@ export class Backend {
     const format = resolveFormat(collection, entry.toJS());
     const fieldsOrder = this.fieldsOrder(collection, entry);
     const fieldsComments = selectFieldsComments(collection, entry);
-    let content = format.toFile(entry.get('data').toJS(), fieldsOrder, fieldsComments);
+    let content = await format.toFile(entry.get('data').toJS(), fieldsOrder, fieldsComments);
     if (content.slice(-1) != '\n') {
       // add the EOL if it does not exist.
       content += '\n';
